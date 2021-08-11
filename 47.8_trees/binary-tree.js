a3e46d42a639a0a58845db675f948a3692572892
Joyce <hellojoycewong@gmail.com>
2021-08-11 14:24:16 -0700

    write/test nextLarger()

--- 47.8_trees/binary-tree.js
+++ 47.8_trees/binary-tree.js
@@ -61,18 +61,22 @@ class BinaryTree {
     //check to see if there is root
     if (this.root === null) return 0;
 
+    //function takes in a node 
     function findDepth(node) {
+      //if there are no children, return 1 for depth
       if (node.left === null && node.right=== null) {
         return 1;
       }
-      //If no left, run findDepth on right
+      //If no left, run findDepth on right(recurse) adding 1 to depth each time
       if (node.left === null) {
         return findDepth(node.right) + 1
       }
+       //If no right, run findDepth on left(recurse) adding 1 to depth each time
       if (node.right === null) {
         return findDepth(node.left) + 1;
       }
-      return (findDepth(node.left) > findDepth(node.right) ? findDepth(node.left) : findDepth(node.right) + 1)
+      //return the value with the higher depth + 1 for root node
+      return (findDepth(node.left) > findDepth(node.right) ? findDepth(node.left) : findDepth(node.right)) + 1
       //If no right, run findDepth on left
     }
     return findDepth(this.root)
@@ -83,6 +87,27 @@ class BinaryTree {
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    //initalize a total;
+    let total = 0;
+    function sum(node) {
+      //check if theres root
+      if (node === null) {
+        return 0;
+      }
+      //recurse the left and right as long as they are not leafs
+      const leftSum = sum(node.left)
+      const rightSum = sum(node.right)
+
+      //total will be the max of either the total, or the val + left * right sum
+      console.log('total',total, node.val + leftSum + rightSum)
+      total = Math.max(total, node.val + leftSum + rightSum);
+
+      return Math.max(0, leftSum + node.val, rightSum + node.val)
+    }
+    
+    sum(this.root)
+    return total;
+  
 
   }
 
@@ -90,9 +115,36 @@ class BinaryTree {
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+      /**we want to go through every single node to check the val searching from top to bottom*/
+      //check if there is anything in the root
+      if (!this.root) return null;
+
+      //check from top to bottom using a queue
+      let queue = [this.root];
+      let nextClosest = null;
+
+      //loop while there is something in thre queue
+      while (queue.length) {
+        //check most recently added val;
+        let current = queue.shift();
+        let currentVal = current.val;
+
+        let higherBoundary = currentVal > lowerBound;
+        let reassignClosest = currentVal < nextClosest || nextClosest === null;
+
+        //if current val is greater than lowerBound and currentVal is less than the nextCloses (or nextClosest is null), reassign the current val to be nextClosest
+        if (higherBoundary && reassignClosest) {
+          nextClosest = currentVal;
+        }
+        //if there is a child node, push the child node into the queue
+        if (current.left) queue.push(current.left)
+        if (current.right) queue.push(current.right)
+
+      }
+
+      return nextClosest;
 
   }
-
   /** Further study!
    * areCousins(node1, node2): determine whether two nodes are cousins
    * (i.e. are at the same level but have different parents. ) */
