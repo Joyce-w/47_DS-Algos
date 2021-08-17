0e241203d81e3c6806e1e82ec1fdb685d8e0a421
Joyce <hellojoycewong@gmail.com>
2021-08-16 17:16:16 -0700

    attempt to write code for pivot

--- 47.11_sorting/quick.js
+++ 47.11_sorting/quick.js
@@ -3,7 +3,38 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+// function pivot(arr) {
+//     //remember the pivot el
+//     let pivotNum = arr[0]
+//     let midIdx = Math.floor(arr.length - 1) / 2;
+//     let midEl = arr[midIdx]
+//     console.log('mid el', midEl)
+//     //swap the pivotNum with the mid value
+//     let temp = pivotNum;
+//     arr[0] = midEl;
+//     console.log(arr[0])
+//     console.log('index of mid',Math.floor(arr.length -1) / 2)
+//     arr[midIdx] = temp;
+//     //
+
+//     return midIdx;
+// }
+
+function pivot(arr) {
+    //remember the pivot el
+    
+    for (let i = 0; i < arr.length; i++){
+        let pivotNum = arr[0];
+        console.log(arr[i], arr)
+        if (arr[i] > pivotNum) {
+            
+        } else { /**arr[i] smaller than pivot number */
+            arr[i]
+        }
+}
+
+    //if valu is greater than
+pivot(arr)
 
 /*
 quickSort accepts an array, left index, and right index
--- 47.11_sorting/selection.js
+++ 47.11_sorting/selection.js
@@ -1,3 +1,30 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    //if no length return arr;
+    if (arr.length <= 1) return arr;
+    //set a minimum as first val
+
+   
+    for (let i = 0; i < arr.length; i++){
+        
+        let min = arr[i];
+         let newMin = arr[i]
+        for (let j = i + 1; j < arr.length; j++){
+            console.log(`min is ${min}, arr[j] is ${arr[j]}`)
+            
+            if (arr[j] < min && arr[j] < newMin) {
+                
+                newMin = arr[j]
+            }
+            console.log(`min is ${min}, newMin is ${newMin}`)
+            arr[j] = min;
+            arr[i] = newMin;
+            console.log(arr)
+        }
+        
+    }
+    return arr;
+}
+
+selectionSort([4, 20, 12, 10, 7, 9])
 
 module.exports = selectionSort;
